7e1fd07f7518609e0ac9aac51553abfab3e2dc72
*romaisa <*mahamkhurram1230@gmail.com>
2024-10-03 18:32:03 +0500

    update

--- class-3/src/app/page.tsx
+++ class-3/src/app/page.tsx
@@ -3,17 +3,18 @@ import Image from "next/image";
 import Footer from "./Footer";
 import image2 from "@/app/image2.png"
 
-
 export default function Home() {
   return (
   <div className="Home">
-    <Header/>  
+    <Header/> 
+
     <Image
     src={image2}
     alt="image2"
     height={200}
     width={1350}
     />
+    
     <Footer/>
   </div>
   );
